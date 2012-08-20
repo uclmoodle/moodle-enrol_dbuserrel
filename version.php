9a23c90cb5bb64109fb538d58c3208d73177167c
MaxCSA <maxime.pelletier@educsa.org>
2012-08-20 13:22:41 -0400

    Updated version.php information

--- version.php
+++ version.php
@@ -11,6 +11,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012061700;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2012082000;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012061700;        // Requires this Moodle version
+$plugin->release   = '0.1';
 $plugin->component = 'enrol_dbuserrel';  // Full name of the plugin (used for diagnostics)
+$plugin->maturity  = MATURITY_BETA;
